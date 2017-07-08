a56eb6fc307c17076a2df5b63b4e752d8e043534
mrinalkrishnanm <mrinal.krishnan06@gmail.com>
2017-07-09 01:08:38 +0530

    goals model added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => :users}
   resources :users, only: [:create,:update,:destroy,:index]
   resources :notes, only: [:create,:update,:destroy,:index,:show]
+  resources :goals, only: [:create,:update,:destroy,:index,:Show]
   post 'tokens/verify', to:'tokens#verify'
   post 'tokens/verify_token', to:'tokens#verify_token'
   post 'notes/other_user', to:'notes#other_user'
