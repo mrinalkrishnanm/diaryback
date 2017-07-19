9c1aa82388880ee6aff5b65fdf6fd0079fe9de66
mrinalkrishnanm <mrinal.krishnan06@gmail.com>
2017-07-19 23:34:01 +0530

    checking for multiple notes on same day

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,4 +2,16 @@ class Note < ActiveRecord::Base
 	belongs_to :User,foreign_key: :user_id
 	has_many :uploads
 	accepts_nested_attributes_for :uploads, allow_destroy: true
+
+	before_save :duplicate_note
+
+	private 
+		def duplicate_note
+			@notes = Note.where('DATE(start) = ?', Date.today).count
+			if @notes.nil?
+				return true
+			else
+				return false
+			end
+		end
 end
